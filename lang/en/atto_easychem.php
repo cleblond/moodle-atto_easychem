183204086e6a93203ef9dd0966664e01178b5061
Carl LeBlond <cleblond@desktop.home>
2014-12-09 23:11:07 -0500

    first commit

---
+++ lang/en/atto_easychem.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'atto_easychem', language 'en'.
+ *
+ * @package    atto_easychem
+ * @copyright  2013 Damyon Wiese  <damyon@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['editeasychem'] = 'Edit easychem using <a href="{$a}" target="_blank">TeX</a>';
+$string['librarygroup1'] = 'Common';
+$string['librarygroup1_desc'] = 'TeX commands listed on the operators tab.';
+$string['librarygroup2'] = 'Rings';
+$string['librarygroup2_desc'] = 'TeX commands listed on the arrows tab.';
+$string['librarygroup3'] = 'Groups';
+$string['librarygroup3_desc'] = 'TeX commands listed on the Greek symbols tab.';
+$string['librarygroup4'] = 'Reactions';
+$string['librarygroup4_desc'] = 'TeX commands listed on the advanced tab.';
+$string['pluginname'] = 'EasyChem';
+$string['preview'] = 'Equation preview';
+$string['cursorinfo'] = 'An arrow indicates the position that new elements from the element library will be inserted.';
+$string['saveeasychem'] = 'Save easychem';
+$string['settings'] = 'Easychem editor settings';
+$string['update'] = 'Update';
